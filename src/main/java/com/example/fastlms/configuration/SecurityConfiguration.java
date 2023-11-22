0f317c8317ea4827821d2268e3cbd69e906fa777
nmskfkai <leejimin00@naver.com>
2023-11-22 11:05:36 +0900

    spring security 성공

--- src/main/java/com/example/fastlms/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/fastlms/configuration/SecurityConfiguration.java
@@ -5,16 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @RequiredArgsConstructor
